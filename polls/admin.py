7fe4dc5ad2f2c07f77e1e98440d9434b9e7929c2
gideongrossman <gideon@velocitek.com>
2015-11-14 14:50:02 -1000

    add models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import Question
+
 
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Here\'s that Date information, dude', {'fields': ['pub_date']}),
+    ]
+
+	
+admin.site.register(Question, QuestionAdmin)
