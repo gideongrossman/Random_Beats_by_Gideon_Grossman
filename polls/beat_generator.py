d28e531642d778d10a7e7bf331cdba58338506d5
gideongrossman <gideon@velocitek.com>
2015-11-16 19:48:16 -1000

    Make more interesting beats

--- polls/beat_generator.py
+++ polls/beat_generator.py
@@ -4,20 +4,19 @@ from random import randint
 def GenerateBeat(number_of_beats_in_measure):
     number_of_snare_hits = randint(1,int(number_of_beats_in_measure))
     number_of_bass_kicks = randint(1,int(number_of_beats_in_measure))
-    beats_to_hit_the_snare = ChooseBeatsToHitDrum(number_of_snare_hits)
-    beats_to_kick_the_bass = ChooseBeatsToHitDrum(number_of_bass_kicks)
+    beats_to_hit_the_snare = ChooseBeatsToHitDrum(number_of_snare_hits, int(number_of_beats_in_measure))
+    beats_to_kick_the_bass = ChooseBeatsToHitDrum(number_of_bass_kicks, int(number_of_beats_in_measure))
     new_beat = Beat(beats_to_kick_the_bass, beats_to_hit_the_snare)
     return new_beat
     
-def ChooseBeatsToHitDrum(number_of_beats):
+def ChooseBeatsToHitDrum(number_of_beats, number_of_beats_in_measure):
     hits = []
     i = 0
     while (i < number_of_beats):
-        count = (randint(1,number_of_beats))
+        count = (randint(1,number_of_beats_in_measure))
         if not(count in hits):
             hits.append(count)
-            i+=1
-                
+            i+=1              
     return hits
     
 class Beat:
